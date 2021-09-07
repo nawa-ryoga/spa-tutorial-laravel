5f585377f60abecb20e21be6060be5c78c5f5f26
nawawa <oonawa12@gmail.com>
2021-09-07 20:18:48 +0900

    タスク一覧取得・詳細閲覧API実装

---
+++ backend/app/Http/Controllers/TaskController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    public function index() {
+        return Task::all();
+    }
+
+    public function show(Task $task) {
+        return $task;
+    }
+}
