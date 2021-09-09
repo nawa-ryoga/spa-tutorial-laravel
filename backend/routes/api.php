c7b2fa9a9b05d819af2be4b52b45e12688853f37
nawawa <oonawa12@gmail.com>
2021-09-09 09:33:49 +0900

    HTTPメソッド名の間違いを修正

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -23,4 +23,4 @@ Route::get('/tasks', [TaskController::class, 'index']);
 Route::post('/tasks', [TaskController::class, 'store']);
 Route::get('/tasks/{task}', [TaskController::class, 'show']);
 Route::put('/tasks/{task}', [TaskController::class, 'update']);
-Route::destroy('/tasks/{task}', [TaskController::class, 'destroy']);
+Route::delete('/tasks/{task}', [TaskController::class, 'destroy']);
